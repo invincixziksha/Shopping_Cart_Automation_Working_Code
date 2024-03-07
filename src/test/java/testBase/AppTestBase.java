bcc3462ca48784b822a9c9fa60ae5dcc49939ce0
sudhansupp <sudhansupanda4u@gmail.com>
2024-03-07 13:04:27 +0530

    updated

--- src/test/java/testBase/AppTestBase.java
+++ src/test/java/testBase/AppTestBase.java
@@ -1,11 +1,14 @@
 package testBase;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.asserts.SoftAssert;
 
 import coreUtilities.testbase.TestBase;
 
 public class AppTestBase extends TestBase {
-	
+
+	public static final Logger logger = LogManager.getLogger();
 	public String projectBaseDirPath = System.getProperty("user.dir");
 	public String config_filePath = projectBaseDirPath+"/src/main/resources/config.json";
 	public String testDataFilePath = projectBaseDirPath+"/src/test/java/testdata/";
